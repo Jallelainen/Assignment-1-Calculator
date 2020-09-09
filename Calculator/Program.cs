9b0db1d23eed418be8e9c7c907583785062e20c8
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-09 13:47:36 +0200

    Made a basic menu, Added a method for ADDITION

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,69 +7,108 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! I'm HAM (Happily Automated Mathematician). " +
-                "How may I be of service?");
+            Console.WriteLine("Welcome! \n" +
+                "I'm HAM (Happily Automated Mathematician). \n " +
+                "How may I be of assistance today?: ");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
 
-            int index = 0;
-
-            List<string> menuItem = new List<string>()
+            bool runMenu = true;
+            do
             {
-                "Addition",
-                "Subtraction",
-                "Multiplication",
-                "Division",
-                "Exit Program"
-            };
-
-            Console.CursorVisible = false;
-            while (true)
-            {
-                for (int i = 0; i < menuItem.Count; i++)
-                {
-                    if (i == index)
-                    {
-                        Console.BackgroundColor = ConsoleColor.Gray;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        
-                        Console.WriteLine(menuItem[i]);
-                    } 
-                    else
-                    {
-                        Console.WriteLine(menuItem[i]);
-                    }
-
-                    Console.ResetColor();
-
-                }
-                
-                ConsoleKeyInfo ckey = Console.ReadKey();
-
-                if(ckey.Key == ConsoleKey.DownArrow)
-                {
-                    if (index == menuItem.Count - 1) 
-                    {
-                        index = 0; 
-                    }
-                    else 
-                    {
-                        index++;
-                    } 
-                }
-                else if(ckey.Key == ConsoleKey.UpArrow)
-                {
-                    if (index <= 0)
-                    {
-                        index = menuItem.Count - 1;
-                    } 
-                    else
-                    {
-                        index--;
-                    }
-                }
-
                 Console.Clear();
+                Console.Write("--------------Calculator Menu--------------\n" +
+                    "Enter 1 for Addition. \n" +
+                    "Enter 2 for Subtracion.\n" +
+                    "Enter 3 for Multiplication\n" +
+                    "Enter 4 for Division.\n" +
+                    "Enter Quit to exit.\n" +
+                    "Selection:");
+
+                string userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "1":
+                       Addition();
+                        break;
+
+                    case "2":
+                        //Subtraction();
+
+                        break;
+
+                    case "3":
+                        //Multiplication();
+                        break;
+
+                    case "4":
+                        //Division();
+                        break;
+                   
+                    case "Quit":
+                        runMenu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Not a valid input. Please try again.");
+                        Console.WriteLine("Press any key to continue");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+
             }
+            while (runMenu);
+
+
+
 
         }
+
+         static int TextToNumber(string textNumber)
+         {
+             int number = 0;
+
+             int.TryParse(textNumber, out number);
+
+             return number;
+         }
+        static int Addition()
+        {
+            int numberOne;
+            int numberTwo;
+            int additionResult;
+
+            Console.WriteLine("Please enter a number: ");
+            numberOne = TextToNumber(Console.ReadLine());
+
+
+            Console.WriteLine("Please enter the number that you wish to add: ");
+            numberTwo = TextToNumber(Console.ReadLine());
+
+            additionResult = numberOne + numberTwo;
+
+            Console.Write("That adds up to:" + additionResult);
+            
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+            
+            return additionResult;
+        }
+
+
+       /* static int Subtraction()
+        {
+
+        } 
+        static int Multiplication()
+        {
+
+        } 
+        static float Division()
+        {
+
+        }*/
     }
 }
